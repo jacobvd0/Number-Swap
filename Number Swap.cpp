2a116b82c4c5a768589c277009adaff00d0c7854
Jacob <46667568+jacobvd0@users.noreply.github.com>
2024-01-31 15:51:49 +1030

    Create Number Swap.cpp

---
+++ Number Swap.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main()
+{
+    int x = 13;
+    int y = 24;
+    
+    // <Your work goes here>
+    
+    int tempNum = x;
+    x = y;
+    y = tempNum;
+    
+    std::cout << "Number Swap)" << std::endl;
+    std::cout << "x is " << x << std::endl;
+    std::cout << "y is " << y << std::endl;
+}
